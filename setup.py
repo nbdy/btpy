73d1d17773d34ff1c4c839d473222541cde8b461
Pascal Eberlein <pascal@eberlein.io>
2021-04-07 19:34:18 +0200

    version bump, install script update

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
 from setuptools.command.install import install
 from setuptools import setup, find_packages
+from os import system
 
 
 class InstallSetupScript(install):
     def run(self):
         install.run(self)
-        try:
-            self.spawn(['sudo', 'apt-get', 'install', '-y',
-                        'python3', 'python3-dev', 'python3-pip',
-                        'libglib2.0-dev', 'libbluetooth-dev',
-                        'libreadline-dev', 'libboost-python-dev', 'git',
-                        'libboost-thread-dev', 'pkg-config', 'python3-bluez'])
-            self.spawn(['cd', '/tmp', ';',
-                        'git', 'clone', '', ';',
-                        'cd', 'gattlib', ';',
-                        'mkdir', 'build', ';',
-                        'cd', 'build', ';',
-                        'cmake', '..', '-DGATTLIB_BUILD_DOCS=OFF',
-                        'make', '-j', '$(nproc)',
-                        'sudo', 'make', 'install'])
-        except Exception as e:
-            print(e)
+        system("sudo apt install -y python3 python3-dev python3-pip libglib2.0-dev libbluetooth-dev git "
+               "libreadline-dev libboost-python-dev libboost-thread-dev pkg-config python3-bluez")
+
+        system("cd /tmp; git clone https://github.com/labapart/gattlib; cd gattlib; mkdir build; cd build; "
+               "cmake .. -DGATTLIB_BUILD_DOCS=OFF; make -j $(nproc); sudo make install")
 
 
 setup(
     long_description=open("README.md", "r").read(),
     name="btpy",
-    version="1.2.6",
+    version="1.2.7",
     description="bluetooth library",
     author="Pascal Eberlein",
     author_email="pascal@eberlein.io",
