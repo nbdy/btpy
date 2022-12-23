eb56273dffc876998d2595cb38e2d8bbcb1fe9f7
Pascal Eberlein <pascal@eberlein.io>
2022-12-23 07:01:56 +0100

    hardcoded pybluez version

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from setuptools import setup, find_packages
 setup(
     long_description=open("README.md", "r").read(),
     name="btpy",
-    version="2.0.2",
+    version="2.0.3",
     description="bluetooth library for beacons, classic and low-energy devices",
     author="Pascal Eberlein",
     author_email="pascal@eberlein.io",
     url="https://github.com/nbdy/btpy",
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
     keywords="bluetooth library",
@@ -24,7 +21,7 @@ setup(
         ]
     },
     install_requires=[
-        "PyBluez", "bleak", "beacontools[scan]"
+        "PyBluez==0.22", "bleak", "beacontools[scan]"
     ],
     long_description_content_type="text/markdown"
 )
